c12789cb92643ac6dcc9c56629a6c68e920886fa
SaveState <pgattic@gmail.com>
2019-09-25 22:10:59 -0400

    fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	if($("#wrapfabtest").height() < 0) {
-		
+		let adBlocker = false;
 	} else {
 		alert("Hey, I noticed that you're using an ad blocker. If you want to show your support for my work, please whitelist this site, I promise I won't blow your computer up with ads!");
 	}
