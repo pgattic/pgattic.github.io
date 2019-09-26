184c6784856139cb897aedc754e08392f635d806
SaveState <pgattic@gmail.com>
2019-09-25 22:08:14 -0400

    adblock detector fix

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	if($("#wrapfabtest").height() < 0) {
+		
+	} else {
+		alert("Hey, I noticed that you're using an ad blocker. If you want to show your support for my work, please whitelist this site, I promise I won't blow your computer up with ads!");
+	}
+});
