8fef841bc5cb8d0e0a7a476e1afe60a1502c3fec
SaveState <pgattic@gmail.com>
2020-06-23 02:15:56 -0400

    Update engine.js

--- games/snake-evolved/engine.js
+++ games/snake-evolved/engine.js
@@ -21,7 +21,7 @@ const
 	compassColor = "#000",
 	pauseKey = "Escape",
 	spectatorRotationVelocity = 0.005,
-	version = "Copyright SaveState. v1.6.3";
+	version = "Copyright SaveState. v1.6.4";
 
 var
 	indexOfSpectate = 1,
@@ -323,8 +323,8 @@ function spawnFood() {
 function constrict(e) {
 	var playerCopy = players[e];
 	for (var i = 1; i < players[e].location.length - 1; i++) {
-		players[e].location[i][0] = (((playerCopy.location[i - 1][0] + playerCopy.location[i + 1][0]) / 2) + players[e].location[i][0]) / 2;
-		players[e].location[i][1] = (((playerCopy.location[i - 1][1] + playerCopy.location[i + 1][1]) / 2) + players[e].location[i][1]) / 2;
+		players[e].location[i][0] = (((playerCopy.location[i - 1][0] + playerCopy.location[i + 1][0]) / 2);// + players[e].location[i][0]) / 2;
+		players[e].location[i][1] = (((playerCopy.location[i - 1][1] + playerCopy.location[i + 1][1]) / 2);// + players[e].location[i][1]) / 2;
 	}
 }
 
