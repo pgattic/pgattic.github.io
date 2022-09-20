6134dd1eeafd63bf6430f695ec964470b4ee0d09
Preston Corless <pgattic@gmail.com>
2022-09-19 23:21:44 -0600

    Complete website rework
    I just... started over, haha. It has been great having the website as it was, but I think it was time for a refresh.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const
+	$=(x)=>{return document.querySelector(x)},
+	rootColors = $(":root").style,
+	
+	cScheme = ["light", "dark"],
+	textColor = ["#444", "#ccc"],
+	bgColor = ["#fff","#111"],
+	navColor = ["#eee","#222"],
+	navHover = ["#bdb","#343"],
+	navSelected = ["#ccc","#333"],
+	navBorder = ["#7c7", "#585"],
+	linkColor = ["#5a5", "#7b7"],
+	shadowColor = ["#aaa", "#000"],
+	btnEmoji = ["☀️","🌙"];
+
+var
+	darkMode = Number(localStorage.getItem("darkMode")) || 0; // 0 for light mode, 1 for dark mode
+
+function refreshColors() {
+	var i = Number(darkMode);
+	$(":root").style="color-scheme: " + cScheme[i];
+	rootColors.setProperty("--text-color", textColor[i]);
+	rootColors.setProperty("--bg-color", bgColor[i]);
+	rootColors.setProperty("--nav-color", navColor[i]);
+	rootColors.setProperty("--nav-hover", navHover[i]);
+	rootColors.setProperty("--nav-selected", navSelected[i]);
+	rootColors.setProperty("--nav-border", navBorder[i]);
+	rootColors.setProperty("--link-color", linkColor[i]);
+	rootColors.setProperty("--shadow-color", shadowColor[i]);
+	$("#darkBtn").innerText = btnEmoji[darkMode];
+	for (var e=0; e<document.getElementsByClassName("extern-nav").length; e++) {
+		document.getElementsByClassName("extern-nav")[e].src = (i==0?"svg/external-link-light.svg":"svg/external-link-dark.svg");
+	}
+}
+
+$("#darkBtn").onclick = (e) => {
+	darkMode = Number(!darkMode);
+	localStorage.setItem("darkMode", darkMode);
+	refreshColors();
+}
+
+refreshColors();
+
+
+document.body.style="margin-top: " + ($("#header").offsetHeight) + "px";
+
+window.onresize = () => {
+	document.body.style="margin-top: " + ($("#header").offsetHeight) + "px";
+}
+
+$("#year").innerText = new Date().getFullYear();
+
--- readme.md
+++
@@ -1,8 +0,0 @@
-# [SaveState's Web Games](https://pgattic.github.io)
-Hello, people! welcome to my website source code!
-
-I would just like to say that all my games and other stuff is free for the taking and copying into any of your websites, but I **require** that you give credit to me, including a link to where it can be found on my website!
-
-I have worked really hard on these things, so that is why I ask that you give credit where it belongs.
-
-I don't really have any other remarks to make here other than [go play my games](https://pgattic.github.io)!
